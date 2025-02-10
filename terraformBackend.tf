4283f105da25fda32571b82d3c494aa8c0b05c2c
EmersonUrielVargas <emrvargaitaz@gmail.com>
2025-02-10 13:21:23 -0500

    remove unused vars

--- terraformBackend.tf
+++ terraformBackend.tf
@@ -4,27 +4,5 @@ terraform {
     storage_account_name = "storageacc00ag"
     container_name       = "terraform-states"
     key                  = "terraform/terraform.tfstate"
-
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    subscription_id = var.subscription_id
-    tenant_id       = var.tenant_id
   }
-}
-
-variable "azure_client_id" {
-  type = string
-}
-
-variable "azure_client_secret" {
-  type = string
-  sensitive = true
-}
-
-variable "azure_subscription_id" {
-  type = string
-}
-
-variable "azure_tenant_id" {
-  type = string
 }
