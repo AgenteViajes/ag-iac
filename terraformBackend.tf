037a11b08cc3fffc80007c3bc01c398504d56d87
EmersonUrielVargas <emrvargaitaz@gmail.com>
2025-02-10 12:30:51 -0500

    remove credentials

--- terraformBackend.tf
+++ terraformBackend.tf
@@ -4,10 +4,5 @@ terraform {
     storage_account_name = "storageacc00ag"
     container_name       = "terraform-states"
     key                  = "terraform/terraform.tfstate"
-
-    subscription_id = var.azure_subscription_id
-    tenant_id       = var.azure_tenant_id
-    client_id       = var.azure_client_id
-    client_secret   = var.azure_client_secret
   }
 }
